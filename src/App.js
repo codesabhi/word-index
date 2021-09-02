70235693e793c01cfa6bd95f5471c54d53b5d92d
Abhishek <yadavabhishek241296@gmail.com>
2021-09-02 11:35:19 +0530

    logs cleared

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Container, Switch, withStyles } from "@material-ui/core";
-import './App.css'
+import "./App.css";
 import Header from "./components/Headers/Header";
 import Definitions from "./components/definitions/Definitions";
 import { grey } from "@material-ui/core/colors";
 
 const App = () => {
 
-  const [word, setWord] = useState("")
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+  const [category, setcategory] = useState("en");
+  const [lightMode, setlightMode] = useState(false);
 
-  const [meanings, setMeanings] = useState([])
 
-  const [category, setcategory] = useState("en")
-
-  const [lightMode, setlightMode] = useState(false)
 
   const DarkMode = withStyles({
     switchBase: {
       color: grey[300],
-      '&$checked': {
+      "&$checked": {
         color: grey[500],
       },
-      '&$checked + $track': {
+      "&$checked + $track": {
         backgroundColor: grey[500],
       },
     },
@@ -32,33 +31,55 @@ const App = () => {
 
   const dictionaryApi = async () => {
     try {
-      const {data} = await axios.get(
+      const { data } = await axios.get(
         `https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`
       );
-      // console.log(data);
-      setMeanings(data)
+      setMeanings(data);
     } catch (error) {
       console.log(error);
     }
   };
-  // console.log(meanings);
-
+  
   useEffect(() => {
     dictionaryApi();
   }, [word, category]);
 
-  return <div className="App" style={{backgroundColor: lightMode? "#fff" : "#282c34", color: lightMode? "black" : "white", transition: "all 0.5s linear"}}>
-    <Container maxWidth="md"
-    className='container'
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundColor: lightMode ? "#fff" : "#282c34",
+        color: lightMode ? "black" : "white",
+        transition: "all 0.5s linear",
+      }}
     >
-    <div style={{position:"absolute", top: 0, right: 15, paddingTop: 10}}>
-    <DarkMode checked={lightMode} onChange={() => setlightMode(!lightMode)} />
-
+      <Container maxWidth="md" className="container">
+        <div
+          style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}
+        >
+          <DarkMode
+            checked={lightMode}
+            onChange={() => setlightMode(!lightMode)}
+          />
+        </div>
+        <Header
+          category={category}
+          setcategory={setcategory}
+          word={word}
+          setWord={setWord}
+          lightMode={lightMode}
+        />
+        {meanings && (
+          <Definitions
+            word={word}
+            meanings={meanings}
+            category={category}
+            lightMode={lightMode}
+          />
+        )}
+      </Container>
     </div>
-      <Header category={category} setcategory={setcategory} word={word} setWord={setWord} lightMode= {lightMode}/>
-      {meanings && (<Definitions word={word} meanings={meanings} category={category} lightMode={lightMode} /> )}
-    </Container>
-  </div>
+  );
 };
 
 export default App;
--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -1,53 +1,55 @@
 import React from "react";
 import "./Header.css";
-import { createTheme, MenuItem, TextField, ThemeProvider } from "@material-ui/core";
+import {
+  createTheme,
+  MenuItem,
+  TextField,
+  ThemeProvider,
+} from "@material-ui/core";
 import categories from "../../data/Category";
 
-const Header = ({category, setcategory, word, setWord, lightMode}) => {
+const Header = ({ category, setcategory, word, setWord, lightMode }) => {
+
+  //ThemeChange
   const darkTheme = createTheme({
     palette: {
       primary: {
-        main:  lightMode? "#000" : "#fff",
+        main: lightMode ? "#000" : "#fff",
       },
-      type: lightMode? "light" : "dark",
+      type: lightMode ? "light" : "dark",
     },
   });
 
   const handleChange = (language) => {
-      setcategory(language);
-      setWord("")
-
-  }
+    setcategory(language);
+    setWord("");
+  };
 
   return (
     <div className="Header">
-      <span className="title"> {word? word : "word hunt"} </span>
+      <span className="title"> {word ? word : "word book"} </span>
       <div className="inputs">
         <ThemeProvider theme={darkTheme}>
-          <TextField 
-          className="search"  
-          label="Word"
-          value={word}
-          onChange={(e) => setWord(e.target.value)}
-           />
+          <TextField
+            className="search"
+            label="Word"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+          />
 
           <TextField
-          className="select"
-        //   id="standard-select-currency"
-          select
-          label="Language"
-          value={category}
-          onChange={(e) => handleChange(e.target.value)}
-        >
-        {categories.map((options) => (
-            <MenuItem key={options.label} value={options.label} >
-              {options.value}
-            </MenuItem>
-        ))}
-         
-            
-         
-        </TextField>
+            className="select"
+            select
+            label="Language"
+            value={category}
+            onChange={(e) => handleChange(e.target.value)}
+          >
+            {categories.map((options) => (
+              <MenuItem key={options.label} value={options.label}>
+                {options.value}
+              </MenuItem>
+            ))}
+          </TextField>
         </ThemeProvider>
       </div>
     </div>
